3d91343a3df21e9b074631823c39da2c6714cf97
akshika-6 <akshika27ak@gmail.com>
2025-08-19 12:29:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -12,90 +12,47 @@ class Node{
 */
 
 class Solution {
-    // Function to return a list of nodes visible from the top view
-    // from left to right in Binary Tree.
     
-    static class Pair {
-        Node node;
-        int hd; // horizontal distance
-        Pair(Node node, int hd) {
-            this.node = node;
-            this.hd = hd;
-        }
-    }
-    static ArrayList<Integer> topView(Node root) {
-        // code here
-        ArrayList<Integer> ans = new ArrayList<>();
-        if (root == null) return ans;
-        
-        Map<Integer, Integer> map = new HashMap<>(); // hd -> node value
-        Queue<Pair> q = new LinkedList<>();
-        
-        int minHD = 0, maxHD = 0;
-        
-        q.offer(new Pair(root, 0));
-        
-        while (!q.isEmpty()) {
-            Pair p = q.poll();
-            Node node = p.node;
-            int hd = p.hd;
-            
-            // Only take the first node for this hd
-            if (!map.containsKey(hd)) {
-                map.put(hd, node.data);
-            }
-            
-            minHD = Math.min(minHD, hd);
-            maxHD = Math.max(maxHD, hd);
-            
-            if (node.left != null) {
-                q.offer(new Pair(node.left, hd - 1));
-            }
-            if (node.right != null) {
-                q.offer(new Pair(node.right, hd + 1));
-            }
-        }
-        
-        // Collect results from leftmost hd to rightmost hd
-        for (int i = minHD; i <= maxHD; i++) {
-            ans.add(map.get(i));
-        }
-        
-        return ans;
+     static class Pair {
+    Node node;
+    int hd;
+    Pair(Node node, int hd) {
+        this.node = node;
+        this.hd = hd;
     }
 }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//         TreeMap<Integer, Integer> map = new TreeMap<>();
-//         m_pre(root, 0, map);
-//         return new ArrayList<>(map.values());
-//     }
-    
-//     public static void m_pre(Node root, int hd, TreeMap<Integer, Integer> map) {
-//         if (root != null) {
-//             // Only add the first node we encounter at this horizontal distance
-//             if (!map.containsKey(hd)) {
-//                 map.put(hd, root.data);
-//             }
-//             m_pre(root.left, hd - 1, map);
-//             m_pre(root.right, hd + 1, map);
-//         }
-//     }
-// }
+ static void helper(Node root, int hd, TreeMap<Integer,ArrayList<Integer>> map) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root, hd));
+        while(!q.isEmpty()) {
+            Pair n = q.poll();
+            Node curr = n.node;
+            int val = n.hd;
+            if(!map.containsKey(val)) {
+                map.put(val, new ArrayList<>());
+                map.get(val).add(curr.data);
+            }
+            
+            if(curr.left!=null) {
+                q.add(new Pair(curr.left, val-1));
+            }
+            if(n.node.right!=null) {
+                q.add(new Pair(curr.right, val+1));
+            }
+        }
+    }
+
+    static ArrayList<Integer> topView(Node root) {
+        // code here
+        TreeMap<Integer,ArrayList<Integer>> map = new TreeMap<>();
+        /*modifiedPreorder(root, 0, map);
+        return new ArrayList<>(map.values());*/
+        helper(root, 0, map);
+        ArrayList<Integer> list = new ArrayList<>();
+        for(Map.Entry<Integer, ArrayList<Integer>>  e: map.entrySet()) {
+            ArrayList<Integer> l = e.getValue();
+            list.add(l.get(0));
+        }
+        return list;
+    }
+}
