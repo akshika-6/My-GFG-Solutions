bcf6a1b3501026d6a4f84a290476410f7b238005
akshika-6 <akshika27ak@gmail.com>
2025-11-09 11:54:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -1,20 +1,18 @@
-// User function Template for Java
 class Solution {
     int minCost(int[] height) {
         // code here
         int n=height.length;
-    int prev=0;
-  int prev2=0;
-  for(int i=1;i<n;i++){
-      
-      int jumpTwo = Integer.MAX_VALUE;
-      int jumpOne= prev + Math.abs(height[i]-height[i-1]);
-      if(i>1)
-        jumpTwo = prev2 + Math.abs(height[i]-height[i-2]);
-    
-      int cur_i=Math.min(jumpOne, jumpTwo);
-      prev2=prev;
-      prev=cur_i;
+        int prev=0;
+        int prev2=0;
+        for(int i=1;i<n;i++){
+            int jump2=Integer.MAX_VALUE;
+            int jump1=prev+Math.abs(height[i]-height[i-1]);
+            if(i>1){
+                jump2=prev2+Math.abs(height[i]-height[i-2]);
+            }
+            int curr=Math.min(jump1,jump2);
+            prev2=prev;
+            prev=curr;
         }
         return prev;
     }
