16728f05a6fbd9b0eaf8c77d78684a60f4c6e107
akshika-6 <akshika27ak@gmail.com>
2025-11-09 22:00:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.java
+++ Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.java
@@ -4,24 +4,13 @@ class Solution {
     int findMaxSum(int arr[]) {
         // code here
         int n=arr.length;
-        int[] m=new int[n];
-        Arrays.fill(m,-1);
-        int c = helper(0,n,m,arr);
-        return c;
-    }
-    public static int helper(int i,int n,int[] m,int[] arr){
-        if(i == n){
-            return m[n-1];
+        if(n == 1) return arr[0];
+        int[] dp=new int[n];
+        dp[0]=arr[0];
+        dp[1]=Math.max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(arr[i]+dp[i-2],dp[i-1]);
         }
-        if(i == 0){
-            m[0] = arr[i];
-            return helper(i+1,n,m,arr);
-        }
-        if(i == 1){
-            m[1] = Math.max(arr[0],arr[1]);
-            return helper(i+1,n,m,arr);
-        }
-        m[i]=Math.max(m[i-2]+arr[i],m[i-1]);
-        return helper(i+1,n,m,arr);
+        return dp[n-1];
     }
 }
