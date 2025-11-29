8d6bea6e9ac232a1de1337d26af382ac569175ad
akshika-6 <akshika27ak@gmail.com>
2025-11-30 00:34:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Adding One/adding-one.java
+++ Difficulty: Easy/Adding One/adding-one.java
@@ -4,26 +4,18 @@ class Solution {
     // Function for adding one to the number represented by the array
     Vector<Integer> addOne(int[] arr) {
         // code here
-        Vector<Integer> result = new Vector<>();
-        int n = arr.length;
-        
-        int carry = 1; // since we have to add 1
-        
-        // Traverse from the end
-        for (int i = n - 1; i >= 0; i--) {
+        Vector<Integer> res=new Vector<>();
+        int n=arr.length;
+        int carry = 1;
+        for(int i=n-1;i>=0;i--){
             int sum = arr[i] + carry;
-            result.add(sum % 10); // store the last digit
-            carry = sum / 10;     // carry will be 1 if sum = 10
+            res.add(sum%10);
+            carry=sum/10;
         }
-        
-        // If carry is left (like in case of 999 → 1000)
-        if (carry > 0) {
-            result.add(carry);
+        if(carry > 0){
+            res.add(carry);
         }
-        
-        // Now result is reversed (because we added from the end)
-        Collections.reverse(result);
-        
-        return result;
+        Collections.reverse(res);
+        return res;
     }
 }
